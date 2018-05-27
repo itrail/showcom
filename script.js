a850fb11c6dc865cadf8eb52e5d7d1b7efb8ba1b
itrail <38611732+itrail@users.noreply.github.com>
2018-05-27 21:28:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function Show()
+{
+    var div = document.getElementById("komunikat");
+    div.style.visibility = 'visible';
+    var napis = "Czy napewno chcesz usunąć?"
+    document.getElementById("tresc").innerHTML = napis;
+}
